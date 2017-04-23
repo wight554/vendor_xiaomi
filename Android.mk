8a7c26e0e700bba534c79d78b201d05a12dabf5b
xyyx <xyyx@mail.ru>
2017-04-23 10:59:33 +0800

    Remove useless fingerprint lib

--- Android.mk
+++ Android.mk
@@ -102,17 +102,6 @@ LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := FingerprintServiceExtension
-LOCAL_MODULE_OWNER := kenzo
-LOCAL_SRC_FILES := proprietary/priv-app/FingerprintServiceExtension/FingerprintServiceExtension.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := kenzo
 LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
