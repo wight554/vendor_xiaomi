1e904a38f8e0cabd87a8177e54bc7e2d035c44c2
MOVZX <movzx@yahoo.com>
2017-07-24 23:07:01 +0300

    kenzo: Add missing perfd profiles
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- vendor-blobs.mk
+++ vendor-blobs.mk
@@ -32,6 +32,8 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/kenzo/propri
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/kenzo/proprietary/vendor/bin,system/vendor/bin)
 
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/kenzo/proprietary/vendor/etc,system/vendor/etc)
+
 PRODUCT_COPY_FILES += vendor/xiaomi/kenzo/proprietary/vendor/qcril.db:system/vendor/qcril.db
 
 PRODUCT_COPY_FILES += vendor/xiaomi/kenzo/proprietary/vendor/etc/public.libraries.txt:system/vendor/etc/public.libraries.txt
