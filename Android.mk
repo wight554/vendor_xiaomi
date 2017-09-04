1161ca019c693b21ca7576aba65125951e8588ec
xyyx <xyyx@mail.ru>
2017-09-04 11:42:23 +0800

    Revert "Add Turbo.apk for Battery Alert"
    This reverts commit 62d1916abd57a3a1e22ca6eb2782270c21e23577.

--- Android.mk
+++ Android.mk
@@ -37,17 +37,6 @@ LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Turbo
-LOCAL_MODULE_OWNER := google
-LOCAL_SRC_FILES := proprietary/priv-app/Turbo/Turbo.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := kenzo
 LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
